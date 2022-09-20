a777cd8c0f9fadb4cffff5801f2c4f8bafde5a8f
Michal Sikora <micsik18@gmail.com>
2022-09-20 23:27:19 +0200

    Modified query 4

--- Round_1_query_4.sql
+++ Round_1_query_4.sql
@@ -1,6 +1,9 @@
 SELECT
-host_id,
-COUNT(neighbourhood) as count_neighbourhood
+COUNT(host_id)
 FROM Listings
-GROUP BY host_id
-HAVING COUNT(neighbourhood) > 1
+WHERE host_id in(
+                SELECT
+                host_id
+                FROM Listings
+                GROUP BY host_id
+                HAVING COUNT(neighbourhood) > 1)
