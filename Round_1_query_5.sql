ab255185f2ae66f2e96984364ea48e403f2e9ea2
Michal Sikora <micsik18@gmail.com>
2022-09-25 23:13:41 +0200

    Modifications in query 5

--- Round_1_query_5.sql
+++ Round_1_query_5.sql
@@ -5,20 +5,20 @@ CTE_ALL AS (
     FROM Listings),
 CTE_entire_place AS (
     SELECT
-    COUNT(listing_id) AS entire_place
+    COUNT(listing_id) AS entire_places
     FROM Listings
     WHERE room_type = 'Entire place'),
 CTE_private_room AS (
     SELECT
-    COUNT(listing_id) AS private_room
+    COUNT(listing_id) AS private_rooms
     FROM Listings
-    WHERE room_type = 'Private room')
+    WHERE room_type = 'Private room' OR room_type = 'Shared room' OR room_type = 'Hotel room')
 SELECT
 CTE_ALL.all_places,
-CTE_entire_place.entire_place,
-CTE_private_room.private_room,
-ROUND(100 * CAST(CTE_entire_place.entire_place AS FLOAT) / CAST(CTE_ALL.all_places AS FLOAT), 2) AS percent_of_entire_places,
-ROUND(100 * CAST(CTE_private_room.private_room AS FLOAT) / CAST(CTE_ALL.all_places AS FLOAT), 2) AS percent_of_private_rooms
+CTE_entire_place.entire_places,
+CTE_private_room.private_rooms,
+ROUND(100 * CAST(CTE_entire_place.entire_places AS FLOAT) / CAST(CTE_ALL.all_places AS FLOAT), 2) AS percent_of_entire_places,
+ROUND(100 * CAST(CTE_private_room.private_rooms AS FLOAT) / CAST(CTE_ALL.all_places AS FLOAT), 2) AS percent_of_private_rooms
 FROM
 CTE_ALL,
 CTE_entire_place,
