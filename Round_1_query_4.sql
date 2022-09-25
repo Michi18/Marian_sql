6cc6d633c536928820ffcf1c5b3ef2ffc94bbb79
Michal Sikora <micsik18@gmail.com>
2022-09-25 20:15:56 +0200

    Third solution for query 4

--- Round_1_query_4.sql
+++ Round_1_query_4.sql
@@ -16,5 +16,15 @@ FROM (
         SELECT host_id
         FROM Listings
         GROUP BY host_id
-        HAVING COUNT(longitude) > 1 or COUNT(latitude) > 1) as l
+        HAVING COUNT(longitude) > 1 or COUNT(latitude) > 1) as l;
+
+--
+
+WITH CTE_1
+AS(SELECT host_id
+        FROM Listings
+        GROUP BY host_id
+        HAVING COUNT(longitude) > 1 or COUNT(latitude) > 1)
+SELECT COUNT(host_id)
+FROM CTE_1
 
