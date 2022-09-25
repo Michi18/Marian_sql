1cb9a3d137516e5f3bd66f5f8a7ed631c9f81244
Michal Sikora <micsik18@gmail.com>
2022-09-25 18:48:28 +0200

    cOMPLETED QUERY 9

--- Round_1_query_9.sql
+++ Round_1_query_9.sql
@@ -1,8 +1,24 @@
-SELECT TOP 1
-	PERCENTILE_CONT(0.25)
+WITH CTE_R
+AS (
+	SELECT TOP 1
+		PERCENTILE_CONT(0.25)
 		WITHIN GROUP (ORDER BY price)
 		OVER () as p25_rome
-FROM Listings
-WHERE 
-	city = 'Rome' and 
-	property_type = 'Entire apartment'
+	FROM Listings
+	WHERE 
+		city = 'Rome' and 
+		property_type = 'Entire apartment'),
+CTE_NY
+AS (
+	SELECT TOP 1
+		PERCENTILE_CONT(0.25)
+		WITHIN GROUP (ORDER BY price)
+		OVER () as p25_newyork
+	FROM Listings
+	WHERE 
+		city = 'New York' and 
+		property_type = 'Entire apartment')
+SELECT
+CTE_R.p25_rome, CTE_NY.p25_newyork, ABS(CTE_R.p25_rome - CTE_NY.p25_newyork) as difference
+FROM
+CTE_R, CTE_NY
