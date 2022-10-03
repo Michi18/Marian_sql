27bad3b908e417ec147b91f01010b415113ac599
Michal Sikora <micsik18@gmail.com>
2022-10-03 23:11:16 +0200

    Second solution for query 7

--- Round_1_query_7.sql
+++ Round_1_query_7.sql
@@ -19,4 +19,25 @@ CTE_ALL.all_listings,
 ROUND(100 * CAST(CTE_Listings_with_words.listings_with_words AS FLOAT) / CAST(CTE_ALL.all_listings AS FLOAT),2) AS percent_of_listings_with_words
 FROM
 CTE_ALL,
-CTE_Listings_with_words
+CTE_Listings_with_words;
+
+
+
+WITH
+CTE_Listings_with_words AS (
+    SELECT
+    listing_id AS listings_with_words
+    FROM Listings
+    WHERE name like '%nice%'
+    or name like '%charming%'
+    or name like '%lovely%'
+    or name like '%elegant%'
+    or name like '%stylish%'
+    or name like '%great%')
+SELECT
+COUNT(CTE_Listings_with_words.listings_with_words) as listings_with_words,
+COUNT(listing_id) as all_listings,
+ROUND(100 * CAST(COUNT(CTE_Listings_with_words.listings_with_words) AS FLOAT) / CAST(COUNT(listing_id) AS FLOAT),2) AS percent_of_listings_with_words
+FROM
+CTE_Listings_with_words,
+Listings
