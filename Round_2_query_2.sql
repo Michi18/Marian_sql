0a1f412c4648987eb5e195bfffeb5e80c9aac5ab
Michal Sikora <micsik18@gmail.com>
2022-11-07 20:49:18 +0100

    New (I wish it's last) version of query 2

--- Round_2_query_2.sql
+++ Round_2_query_2.sql
@@ -6,7 +6,7 @@ RA AS (
     review_scores_checkin +
     review_scores_communication +
     review_scores_location +
-    review_scores_value AS FLOAT)/6,2) as avg_value
+    review_scores_value AS FLOAT)/6,2) AS avg_value
     FROM Reviews)
 SELECT 
 l.listing_id,
@@ -21,12 +21,12 @@ GROUP BY l.listing_id;
 WITH
 RA AS(
     Select listing_id,
-    AVG(CAST(review_scores_accuracy AS FLOAT)) as AVG_1,
-    AVG(CAST(review_scores_cleanliness AS FLOAT)) as AVG_2,
-    AVG(CAST(review_scores_checkin AS FLOAT)) as AVG_3,
-    AVG(CAST(review_scores_communication AS FLOAT)) as AVG_4,
-    AVG(CAST(review_scores_location AS FLOAT)) as AVG_5,
-    AVG(CAST(review_scores_value AS FLOAT)) as AVG_6
+    AVG(CAST(review_scores_accuracy AS FLOAT)) AS AVG_1,
+    AVG(CAST(review_scores_cleanliness AS FLOAT)) AS AVG_2,
+    AVG(CAST(review_scores_checkin AS FLOAT)) AS AVG_3,
+    AVG(CAST(review_scores_communication AS FLOAT)) AS AVG_4,
+    AVG(CAST(review_scores_location AS FLOAT)) AS AVG_5,
+    AVG(CAST(review_scores_value AS FLOAT)) AS AVG_6
     FROM Reviews
     GROUP BY listing_id)
 SELECT r.listing_id, r.AVG_1, r.AVG_2, r.AVG_3, r.AVG_4, r.AVG_5, r.AVG_6,
@@ -36,19 +36,19 @@ ON r.listing_id = l.listing_id;
 
 
 WITH Revs AS(
-    SELECT listing_id, review_scores_accuracy as rev FROM Reviews
+    SELECT listing_id, CAST(review_scores_accuracy AS FLOAT) AS rev FROM Reviews
     UNION ALL
-    SELECT listing_id, review_scores_cleanliness as rev FROM Reviews
+    SELECT listing_id, CAST(review_scores_cleanliness AS FLOAT) AS rev FROM Reviews
     UNION ALL
-    SELECT listing_id, review_scores_checkin as rev FROM Reviews
+    SELECT listing_id, CAST(review_scores_checkin AS FLOAT) AS rev FROM Reviews
     UNION ALL
-    SELECT listing_id, review_scores_communication as rev FROM Reviews
+    SELECT listing_id, CAST(review_scores_communication AS FLOAT) AS rev FROM Reviews
     UNION ALL
-    SELECT listing_id, review_scores_location as rev FROM Reviews
+    SELECT listing_id, CAST(review_scores_location AS FLOAT) AS rev FROM Reviews
     UNION ALL
-    SELECT listing_id, review_scores_value as rev FROM Reviews)
+    SELECT listing_id, CAST(review_scores_value AS FLOAT) AS rev FROM Reviews)
 SELECT
-l.listing_id, ROUND(AVG(CAST(r.rev AS FLOAT)), 2) as avg_rev
+l.listing_id, ROUND(AVG(r.rev),2) AS avg_rev
 FROM
 Listings l INNER JOIN Revs r
 ON l.listing_id = r.listing_id
