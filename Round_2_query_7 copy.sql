849f8ebbbec3053dba5d9891683de741816c8496
Michal Sikora <micsik18@gmail.com>
2022-11-07 23:39:28 +0100

    A lot to do...

---
+++ Round_2_query_7 copy.sql
@@ -0,0 +1,32 @@
+WITH
+Union_revs AS
+    (SELECT listing_id, review_scores_accuracy AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_cleanliness AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_checkin AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_communication AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_location AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_value AS rev FROM Reviews),
+Percentiles AS
+    (SELECT listing_id,
+    AVG(CAST(review_scores_cleanliness AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_clean,
+    AVG(CAST(review_scores_value AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_value,
+    AVG(CAST(review_scores_checkin AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_checkin,
+    PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY review_scores_cleanliness) OVER () AS P80_clean,
+    PERCENTILE_CONT(0.6) WITHIN GROUP (ORDER BY review_scores_value) OVER () AS P60_value,
+    PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY review_scores_checkin) OVER () AS P25_checkin
+    FROM Reviews)
+SELECT DISTINCT l.listing_id,
+CASE
+    WHEN p.AVG_clean > p.P80_clean THEN 'Super clean apartament'
+    WHEN p.AVG_value > p.P60_value THEN 'Good quality/price relation'
+    WHEN p.AVG_checkin < p.P25_checkin THEN 'Difficult checkin'
+    ELSE 'Common listing'
+END AS TAG
+FROM Listings l LEFT JOIN Percentiles p
+ON l.listing_id = p.listing_id
+ORDER BY l.listing_id
