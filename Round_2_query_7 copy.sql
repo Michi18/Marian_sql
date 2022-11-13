c6c83dad1d54b9bdc35fb22ed9dab95130a31503
Michal Sikora <micsik18@gmail.com>
2022-11-13 10:39:48 +0100

    New ver

--- Round_2_query_7 copy.sql
+++ Round_2_query_7 copy.sql
@@ -12,21 +12,16 @@ Union_revs AS
     UNION ALL
     SELECT listing_id, review_scores_value AS rev FROM Reviews),
 Percentiles AS
-    (SELECT listing_id,
-    AVG(CAST(review_scores_cleanliness AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_clean,
-    AVG(CAST(review_scores_value AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_value,
-    AVG(CAST(review_scores_checkin AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_checkin,
-    PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY review_scores_cleanliness) OVER () AS P80_clean,
-    PERCENTILE_CONT(0.6) WITHIN GROUP (ORDER BY review_scores_value) OVER () AS P60_value,
-    PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY review_scores_checkin) OVER () AS P25_checkin
-    FROM Reviews)
-SELECT DISTINCT l.listing_id,
-CASE
-    WHEN p.AVG_clean > p.P80_clean THEN 'Super clean apartament'
-    WHEN p.AVG_value > p.P60_value THEN 'Good quality/price relation'
-    WHEN p.AVG_checkin < p.P25_checkin THEN 'Difficult checkin'
-    ELSE 'Common listing'
-END AS TAG
-FROM Listings l LEFT JOIN Percentiles p
-ON l.listing_id = p.listing_id
-ORDER BY l.listing_id
+    (SELECT r.listing_id,
+    AVG(CAST(r.review_scores_cleanliness AS FLOAT)) OVER(PARTITION BY r.listing_id) AS AVG_clean,
+    AVG(CAST(r.review_scores_value AS FLOAT)) OVER(PARTITION BY r.listing_id) AS AVG_value,
+    AVG(CAST(r.review_scores_checkin AS FLOAT)) OVER(PARTITION BY r.listing_id) AS AVG_checkin,
+    AVG(CAST(u.rev AS FLOAT)) OVER(PARTITION BY u.listing_id) as AVG_revs,
+    PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY r.review_scores_cleanliness) OVER () AS P80_clean,
+    PERCENTILE_CONT(0.6) WITHIN GROUP (ORDER BY r.review_scores_value) OVER () AS P60_value,
+    PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY r.review_scores_checkin) OVER () AS P25_checkin,
+    PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY u.rev) OVER () AS P70_all
+    FROM Reviews r INNER JOIN Union_revs u
+    ON r.listing_id = u.listing_id) 
+SELECT DISTINCT p.listing_id, p.AVG_clean, p.AVG_value, p.AVG_checkin, p.AVG_revs
+FROM Percentiles p;
--- Round_2_query_7.sql
+++ Round_2_query_7.sql
@@ -1,31 +1,29 @@
-WITH AVG_REVS AS
-    (WITH Revs AS
-        (SELECT listing_id, review_scores_accuracy AS rev FROM Reviews
-        UNION ALL
-        SELECT listing_id, review_scores_cleanliness AS rev FROM Reviews
-        UNION ALL
-        SELECT listing_id, review_scores_checkin AS rev FROM Reviews
-        UNION ALL
-        SELECT listing_id, review_scores_communication AS rev FROM Reviews
-        UNION ALL
-        SELECT listing_id, review_scores_location AS rev FROM Reviews
-        UNION ALL
-        SELECT listing_id, review_scores_value AS rev FROM Reviews)
-    SELECT
-    listing_id, ROUND(AVG(CAST(rev AS FLOAT),2) AS avg_rev
-    FROM
-    Revs),
+WITH
+Union_revs AS
+    (SELECT listing_id, review_scores_accuracy AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_cleanliness AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_checkin AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_communication AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_location AS rev FROM Reviews
+    UNION ALL
+    SELECT listing_id, review_scores_value AS rev FROM Reviews),
 Percentiles AS
-    (SELECT r.listing_id,
-    AVG((r.review_scores_cleanliness) OVER(PARTITION BY r.listing_id) AS AVG_clean,
-    AVG((r.review_scores_value) OVER(PARTITION BY r.listing_id) AS AVG_value,
-    AVG((r.review_scores_checkin) OVER(PARTITION BY r.listing_id) AS AVG_checkin,
-    PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY r.review_scores_cleanliness) OVER () AS P80_clean,
-    PERCENTILE_CONT(0.6) WITHIN GROUP (ORDER BY r.review_scores_value) OVER () AS P60_value,
-    PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY r.review_scores_checkin) OVER () AS P25_checkin,
-    PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY a.avg_rev) OVER() AS P75_avg_rank
-    FROM Reviews r INNER JOIN AVG_REVS a
-    ON r.listing_id = a.listing_id)
+    (SELECT listing_id,
+    AVG(CAST(review_scores_cleanliness AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_clean,
+    AVG(CAST(review_scores_value AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_value,
+    AVG(CAST(review_scores_checkin AS FLOAT)) OVER(PARTITION BY listing_id) AS AVG_checkin,
+    PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY review_scores_cleanliness) OVER () AS P80_clean,
+    PERCENTILE_CONT(0.6) WITHIN GROUP (ORDER BY review_scores_value) OVER () AS P60_value,
+    PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY review_scores_checkin) OVER () AS P25_checkin
+    FROM Reviews),
+AVG_reviews AS(
+    SELECT listing_id,
+    AVG(CAST(rev AS FLOAT)) as AVG_revs
+    FROM Union_revs)
 SELECT DISTINCT l.listing_id,
 CASE
     WHEN p.AVG_clean > p.P80_clean THEN 'Super clean apartament'
@@ -35,4 +33,4 @@ CASE
 END AS TAG
 FROM Listings l LEFT JOIN Percentiles p
 ON l.listing_id = p.listing_id
-ORDER BY l.listing_id;
+ORDER BY l.listing_id
