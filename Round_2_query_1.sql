cfe227788b0ff6b74b14d7e495ace747d73eb3d3
Michal Sikora <micsik18@gmail.com>
2022-11-27 17:53:21 +0100

    Corrected query 1

--- Round_2_query_1.sql
+++ Round_2_query_1.sql
@@ -1,4 +1,11 @@
 SELECT r.review_id ,r.listing_id
 FROM Reviews r LEFT JOIN Listings l
 ON r.listing_id = l.listing_id
-WHERE l.listing_id IS NULL
+WHERE l.listing_id IS NULL;
+
+SELECT l.listing_id, r.listing_id
+FROM Reviews r RIGHT JOIN Listings l
+ON r.listing_id = l.listing_id
+WHERE r.listing_id IS NULL
+
+-- 86156 rows don't match
