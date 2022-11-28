75cc1fe406c659febe72d7bede3d8a9877b871ec
Michal Sikora <micsik18@gmail.com>
2022-11-28 23:49:48 +0100

    Little changes to query 6

--- Round_2_query_6.sql
+++ Round_2_query_6.sql
@@ -7,10 +7,11 @@ YEAR AS(
     FROM Listings
     GROUP BY host_id)
 SELECT
-AVG(CASE WHEN years_of_hosting > 7 THEN listings_count END) AS avg_over_7,
-AVG(CASE WHEN years_of_hosting <= 7 THEN listings_count END) AS avg_under_7,
+ROUND(AVG(CASE WHEN years_of_hosting > 7 THEN listings_count END), 2) AS avg_over_7,
+ROUND(AVG(CASE WHEN years_of_hosting <= 7 THEN listings_count END), 2) AS avg_under_7,
 SUM(CASE WHEN years_of_hosting > 7 THEN listings_count END) AS sum_over_7,
-SUM(CASE WHEN years_of_hosting <= 7 THEN listings_count END) AS sum_under_7
+SUM(CASE WHEN years_of_hosting <= 7 THEN listings_count END) AS sum_under_7,
+ROUND(100*CAST(COUNT(years_of_hosting) AS FLOAT)/CAST(COUNT(*) AS FLOAT), 2) AS percent_with_start_date
 FROM YEAR
 
 -- hosts, who are shorter than 7 years on airbnb, have greater avg and sum of listings
