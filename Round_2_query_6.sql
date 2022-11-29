c8eb76419645eaeaeb19e20ad14a84db00c75f64
Michal Sikora <micsik18@gmail.com>
2022-11-29 23:40:10 +0100

    Corrected query 6

--- Round_2_query_6.sql
+++ Round_2_query_6.sql
@@ -1,17 +1,26 @@
 WITH
-YEAR AS(
+year AS(
     SELECT
         host_id,
         CAST(COUNT(listing_id) AS FLOAT) AS listings_count,
-        AVG(ABS(DATEDIFF(year, GETDATE(), host_since))) AS years_of_hosting
+        AVG(ABS(CAST(DATEDIFF(year, GETDATE(), host_since) AS FLOAT))) AS years_of_hosting
     FROM Listings
-    GROUP BY host_id)
+    GROUP BY host_id),
+seven AS(
+    SELECT
+        host_id,
+        CASE
+            WHEN years_of_hosting > 7 THEN 'Eight or more years'
+            WHEN years_of_hosting <= 7 THEN 'Seven or less years'
+            ELSE NULL
+        END AS exp_group
+    FROM year)
 SELECT
-ROUND(AVG(CASE WHEN years_of_hosting > 7 THEN listings_count END), 2) AS avg_over_7,
-ROUND(AVG(CASE WHEN years_of_hosting <= 7 THEN listings_count END), 2) AS avg_under_7,
-SUM(CASE WHEN years_of_hosting > 7 THEN listings_count END) AS sum_over_7,
-SUM(CASE WHEN years_of_hosting <= 7 THEN listings_count END) AS sum_under_7,
-ROUND(100*CAST(COUNT(years_of_hosting) AS FLOAT)/CAST(COUNT(*) AS FLOAT), 2) AS percent_with_start_date
-FROM YEAR
+s.exp_group,
+ROUND(AVG(y.listings_count), 2) as avg_listings,
+ROUND(COUNT(y.listings_count), 2) as count_listings 
+FROM year y INNER JOIN seven s
+ON y.host_id = s.host_id
+GROUP BY s.exp_group
 
 -- hosts, who are shorter than 7 years on airbnb, have greater avg and sum of listings
