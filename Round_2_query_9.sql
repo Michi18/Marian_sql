77a124db94f23b15c6b105805bec68bf19b442a7
Michal Sikora <micsik18@gmail.com>
2023-01-12 20:01:31 +0100

    Corrected query 9

--- Round_2_query_9.sql
+++ Round_2_query_9.sql
@@ -1,19 +1,27 @@
 WITH
 Revs AS(
         SELECT *,
-        ROW_NUMBER() OVER (PARTITION BY listing_id ORDER BY review_id DESC) AS rn
-        FROM Reviews)
-SELECT 
-        listing_id,
-        review_id,
-        date,
-        reviewer_id,
-        review_scores_accuracy,
-        review_scores_cleanliness,
-        review_scores_checkin,
-        review_scores_communication,
-        review_scores_location,
-        review_scores_value,
-        rn
-FROM Revs
-WHERE rn = 2
+                ROW_NUMBER() OVER (PARTITION BY listing_id ORDER BY review_id DESC) AS rn
+        FROM Reviews),
+R_one AS(
+        SELECT 
+                listing_id,
+                review_id,
+                review_scores_value
+        FROM Revs
+        WHERE rn = 1),
+R_two AS(
+        SELECT 
+                listing_id,
+                review_id,
+                review_scores_value
+        FROM Revs
+        WHERE rn = 2)
+SELECT
+        r1.listing_id,
+        r1.review_id,
+        r2.review_id,
+        r1.review_scores_value AS last_review_scores_value,
+        r2.review_scores_value AS preview_review_scores_value
+FROM R_one r1 LEFT JOIN R_two r2
+ON r1.listing_id = r2.listing_id;
