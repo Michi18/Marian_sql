352cf3038e9b1e78cfcb4e9b538b1458ab4593ea
Michal Sikora <micsik18@gmail.com>
2023-01-23 22:58:23 +0100

    Next stage

--- Round_2_query_10.sql
+++ Round_2_query_10.sql
@@ -9,18 +9,48 @@ CTE_Calendar AS(
         FROM CTE_Calendar
         WHERE DATEADD(dd, 1, [date]) <= @MaxDate),
 CTE_City_Date AS(
-        SELECT DISTINCT l.city, c.[date]
-        FROM CTE_Calendar c CROSS JOIN Listings l 
+        SELECT DISTINCT 
+                l.city, 
+                c.[date]
+        FROM CTE_Calendar c CROSS JOIN Listings l
         OPTION (MAXRECURSION 0)),
-CTE_Whole_Year AS (
+CTE_Without_All_Dates AS(
+        SELECT
+                TOP 100 l.listing_id,
+                r.review_id,
+                l.city,
+                r.date,
+                r.review_scores_checkin
+        FROM Reviews r INNER JOIN Listings l
+        ON r.listing_id = l.listing_id
+        WHERE DATEPART(YY, r.date) = 2018
+        ORDER BY l.city, date DESC),
+CTE_All AS (
+        SELECT
+                w.l.listing_id,
+                w.review_id,
+                city,
+                date,
+                w.review_scores_checkin
+        FROM CTE_City_Date c LEFT JOIN CTE_Withous_All_Dates w
+        ON c.date = w.date)
+SELECT *
+FROM CTE_All;
 
-SELECT
-    l.listing_id,
-    r.review_id,
-    l.city,
-    date,
-    r.review_scores_checkin
-FROM Reviews r INNER JOIN Listings l
-ON r.listing_id = l.listing_id
-WHERE DATEPART(YY, r.date) = 2018
-ORDER BY l.city, date DESC;
+
+
+DECLARE @MinDate DATE = '20180101';
+DECLARE @MaxDate DATE = '20181231';
+
+WITH
+CTE_Calendar AS(
+        SELECT @MinDate as [date]
+        UNION ALL
+        SELECT DATEADD (dd, 1, [date])
+        FROM CTE_Calendar
+        WHERE DATEADD(dd, 1, [date]) <= @MaxDate)
+SELECT DISTINCT 
+        l.city, 
+        c.[date]
+FROM CTE_Calendar c CROSS JOIN Listings l 
+OPTION (MAXRECURSION 0)
